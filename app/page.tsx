7d612c86eb03f9e876aac396adf3fece4765f548
Shema Charmant <shemacharmant@192.168.1.71>
2026-02-16 03:21:00 +0200

    Update: Page.tsx speed-insights vercel

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import { KeyFeatures } from "@/components/sections/KeyFeatures";
 import { PrivacySection } from "@/components/sections/PrivacySection";
 import { RoadmapSection } from "@/components/sections/RoadmapSection";
 import { FAQSection } from "@/components/sections/FAQSection";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <>
+      <SpeedInsights />
       <Hero />
       <HowItWorks />
       <KeyFeatures />
